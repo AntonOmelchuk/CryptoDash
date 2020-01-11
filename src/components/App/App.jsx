8726f09dacf90d9f98afe21a86b8bb3f34dd7250
Anton Omelchuk <kino_kg@ukr.net>
2020-01-11 16:56:37 +0200

    add loading content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,21 @@ import {GlobalStyle} from '../../global.styles';
 import {AppContainer} from './app.style';
 
 import Navbar from '../Navbar/Navbar';
-import WelcomeMessage from '../Settings/index';
-import AppProvider from "./AppProvider";
+import Settings from '../Settings';
+import AppProvider from './AppProvider';
+import Content from '../Shared/Content';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <AppContainer>
-          <AppProvider>
-              <Navbar />
-              <WelcomeMessage />
-          </AppProvider>
+        <AppProvider>
+          <Navbar />
+          <Content>
+            <Settings />
+          </Content>
+        </AppProvider>
       </AppContainer>
     </>
   );
